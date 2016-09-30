eaaad166314d33524717af9c83ecf8ab0ea398e7
Tom Hazledine <tom@eatenbymonsters.com>
2016-09-30 13:36:16 +0100

    syntax fixes for Travis

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,26 +1,16 @@
 <?php
 /**
- * --------------
- * HELPERS
- * 
- * Generic helper
- * functions, not
- * included in _s
- * theme.
- * --------------
+ * Tabula Rasa Helpers
+ *
+ * @package Tabula_Rasa
  */
 
 /**
- * -------------------
- * PRINT PRE
+ * Print Pre: outputs a pre-styled block to make debug prints more easy to read.
  *
- * Output a pre-styled
- * block to make debug
- * prints more easy to
- * read.
- * -------------------
+ * @param  string $stuff_to_print Text to be wrapped in styled pre-tag.
  */
-function print_pre( $stuff_to_print ){
+function print_pre( $stuff_to_print ) {
 	echo '<pre style="
 		background:#ededed;
 		color:#444;
@@ -30,6 +20,6 @@ function print_pre( $stuff_to_print ){
 		padding:10px;
 		margin:10px;
 	">';
-	var_dump( $stuff_to_print );
+	print( $stuff_to_print );
 	echo '</pre>';
 }
