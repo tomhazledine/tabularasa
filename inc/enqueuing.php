2074052b0b69107b827c49b55f7f73c4222115b6
Tom Hazledine <tom@eatenbymonsters.com>
2016-10-01 12:05:34 +0100

    adding gulp and uncompressed assets

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -9,11 +9,9 @@
  * Enqueue scripts and styles.
  */
 function tabularasa_scripts() {
-	wp_enqueue_style( 'tabularasa-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'tabularasa-style', get_stylesheet_uri() . '/assets/css/main.css' );
 
-	wp_enqueue_script( 'tabularasa-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'tabularasa-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'tabularasa-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
